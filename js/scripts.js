e3973c8e439698742fe13b517ec931d4f4709cbd
little-silk <gabbyalvarez@comcast.net>
2022-04-18 23:10:13 -0700

    Color changes and header animation

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,12 @@
 // My Scripts
-console.log('Hello World, this is NOT ME!!!!!');
+const banner = document.querySelector('.banner');
+
+const title = document.querySelector('.title');
+
+const letters = document.querySelectorAll('.letters');
+
+const Time_l = new TimelineMax();
+
+Time_l
+  .fromTo(banner,1,{height:"0%"},{height:"80%",ease: Power2.easeInOut})
+.fromTo(banner, 1.2, {width:"100%"},{width: "80%", ease: Power2.easeInOut}) .staggerFrom(letters, 1, {opacity:0, ease: Back.easeOut.config(1.7)}, 0.2, "-=0.5");
